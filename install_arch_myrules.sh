4408c7e684e8e019ee00a6a9ddc8a31eecb3ce37
りーぱー <119842878+ReaperKun81334102@users.noreply.github.com>
2025-11-24 00:35:17 +0900

    Refine user instructions and enhance installation steps
    Updated notice message for user instructions and added additional setup steps for the Arch Linux installation process.

--- install_arch_myrules.sh
+++ install_arch_myrules.sh
@@ -12,8 +12,7 @@ printf "Enter target device (/dev/sdX) : "
 read TARGET_DEV
 
 printf "\n
-NOTICE: Do not touch the keyboard during the installation. \n"
-
+NOTICE: Do not touch the keyboard during installation until instructed to do so. \n"
 printf "Installation will begin in 5 seconds "
 printf "."
 sleep 1
@@ -98,7 +97,140 @@ pacman -Syy --noconfirm
 # install base packages
 echo "Installing ArchLinux base packages"
 
+mkdir -p "/mnt/etc/" 
+cp -L /etc/resolv.conf "/mnt/etc/resolv.conf" 
 pacstrap /mnt base base-devel btrfs-progs linux linux-firmware \
   terminus-font zsh-completions grml-zsh-config wget aria2 git
 
+
+# setup base system
+echo "setting up base systems"
+
+mkdir -p "/mnt/etc/" 
+cp -L /etc/resolv.conf "/mnt/etc/resolv.conf" 
+
+echo "setup fstab"
+genfstab -U /mnt >> "/mnt/etc/fstab"
+sed 's/relatime/noatime/g' -i "/mnt/etc/fstab"
+
+echo "setup system folder"
+mkdir -p "/mnt/"{proc,sys,dev} 
+mount -t proc proc "/mnt/proc" 
+mount --rbind /sys "/mnt/sys" 
+mount --make-rslave "/mnt/sys" 
+mount --rbind /dev "/mnt/dev" 
+mount --make-rslave "/mnt/dev"
+
+# setup locales
+sed -i "s/^#en_US.UTF-8/en_US.UTF-8/" "/mnt/etc/locale.gen"
+sed -i "s/^#$LOCALE/$LOCALE/" "/mnt/etc/locale.gen"
+chroot /mnt locale-gen 
+echo "LANG=$LOCALE" > "/mnt/etc/locale.conf"
+echo "KEYMAP=$KEYMAP" > "/mnt/etc/vconsole.conf"
+chroot /mnt ln -sf "/usr/share/zoneinfo/Asia/Tokyo" /etc/localtime
+
+# setup chroot env pacman
+cp -rvf /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
+sed -i "s/#ParallelDownloads/ParallelDownloads/" "/mnt/etc/pacman.conf"
+sed -i 's/^#Color/Color/' "/mnt/etc/pacman.conf"
+  if [ "$(uname -m)" = "x86_64" ]
+  then
+    printf "\n\n"
+    if grep -q "#\[multilib\]" "/mnt/etc/pacman.conf"
+    then
+      # it exists but commented
+      sed -i '/\[multilib\]/{ s/^#//; n; s/^#//; }' "/mnt/etc/pacman.conf"
+    elif ! grep -q "\[multilib\]" "/mnt/etc/pacman.conf"
+    then
+      # it does not exist at all
+      printf "[multilib]\nInclude = /mnt/etc/pacman.d/mirrorlist\n" \
+        >> "/etc/pacman.conf"
+    fi
+  fi
+  chroot /mnt pacman -Syy --noconfirm
+
+
+# setup initramfs
+echo 'FONT=ter-114n' >> "/mnt/etc/vconsole.conf"
+chroot /mnt mkinitcpio -P
+
+# setup hostname
+echo "arch" > /mnt/etc/hostname
+
+# setup user
+printf "Enter new username: "
+read user
+echo
+
+chroot /mnt groupadd "$user" 
+chroot /mnt useradd -g "$user" -d "/home/$user" -s "/bin/bash" \
+    -G "$user,wheel,users,video,audio" -m "$user" 
+chroot /mnt chown -R "$user":"$user" "/home/$user" 
+echo "$user ALL=(ALL:ALL) ALL" >> /mnt/etc/sudoers 
+echo "root ALL=(ALL:ALL) ALL" >> /mnt/etc/sudoers
+
+echo "Setup password: "
+chroot /mnt passwd $user
+
+# setup extra packages
+echo "Reinitializing keyring"
+chroot /mnt pacman -S --overwrite='*' --noconfirm archlinux-keyring
+
+echo "installing extra packages"
+arch='arch-install-scripts pkgfile'
+bluetooth='bluez bluez-hid2hci bluez-tools bluez-utils'
+browser='elinks firefox'
+editor='hexedit nano vim'
+filesystem='cifs-utils dmraid exfat-utils f2fs-tools efibootmgr dosfstools
+gpart gptfdisk mtools nilfs-utils ntfs-3g partclone parted partimage'
+filemanager='thunar ark dolphin'
+media='ffmpeg yt-dlp mpv'
+audio='alsa-utils pipewire-pulse pavucontrol'
+hardware='amd-ucode intel-ucode'
+kernel='linux-headers'
+misc='acpi git haveged hdparm htop inotify-tools ipython irssi
+linux-atm lsof mercurial mesa mlocate moreutils p7zip rsynclsb-release
+rtorrent screen scrot smartmontools strace tmux udisks2 unace unrar
+unzip upower usb_modeswitch usbutils zip fcitx5-im fcitx5-mozc python'
+fonts='ttf-dejavu ttf-indic-otf ttf-liberation xorg-fonts-misc unicode-emoji noto-fonts-emoji noto-fonts-cjk noto-fonts-extra'
+network='atftp bind-tools bridge-utils darkhttpd dhclient dhcpcd dialog
+dnscrypt-proxy dnsmasq dnsutils fwbuilder iw networkmanager
+iwd lftp nfs-utils ntp openconnect openssh openvpn ppp pptpclient rfkill
+rp-pppoe socat vpnc wireless_tools wpa_supplicant wvdial xl2tpd'
+xorg='xorg rxvt-unicode xf86-video-amdgpu xf86-video-ati xorg-xclock
+xf86-video-dummy xf86-video-fbdev xf86-video-intel xf86-video-nouveau
+xf86-video-sisusb xf86-video-vesa
+xf86-video-voodoo xorg-server xorg-xbacklight xorg-xinit xterm xorg-xlsfonts'
+desktop="gnome gnome-extra gnome-screenshot gnome-keyring"
+
+all="$arch $bluetooth $browser $editor $filesystem $fonts $hardware $kernel"
+all="$all $misc $network $xorg $audio $media $filemanager $desktop"
+
+chroot /mnt pacman -Sy --noconfirm --needed --overwrite="*" $all
+
+
+# setup systemd
+echo "setup network"
+chroot /mnt systemctl enable iwd NetworkManager gdm
+
+# setup configs
+printf "
+GTK_IM_MODULE=fcitx5 \n
+QT_IM_MODULE=fcitx5 \n
+XMODIFIERS=@im=fcitx5 \n" > /mnt/etc/environment
+
+# setup boot loader
+echo "installing bootloader"
+
+chroot /mnt grub-install --target="x86_64-efi" --efi-directory=/boot --bootloader-id="ArchLinux(x86_64)" --recheck ${TARGET_DEV}
+chroot /mnt grub-mkconfig -o /boot/grub/grub.cfg
+
+
+# finish
+echo "updating initramfs"
+chroot /mnt mkinitcpio -P
+
+sync
+
+echo "Install complete!"
 exit 0
